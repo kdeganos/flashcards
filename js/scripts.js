fd61dd28b83000421d6c7fc5bdcaab96fdcb2dd1
John Dow and Kevin Deganos <kevin@>
2016-03-22 13:07:06 -0700

    Changed to ids for hiding or showing definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 $(function(){
-  $(".term").click(function() {
-    $("#javaScript p").toggle("definition");
-    $("#javaScript h3").toggle("disappear");
+  $("h3#javascriptterm").click(function() {
+    $("p#javascriptdefinition").removeClass("definition");
+    $("h3#javascriptterm").addClass("disappear");
   });
 
-  $(".definition").click(function() {
-    $("#javaScript h3").toggle("disappear");
-    $("#javaScript p").toggle("definition");
+  $("p#javascriptdefinition").click(function() {
+    $("h3#javascriptterm").removeClass("disappear");
+    $("p#javascriptdefinition").addClass("definition");
   });
 
-  $(".term").click(function() {
-    $("#operators p").toggle("definition");
-    $("#operators h3").toggle("disappear");
+  $("h3#operatorsterm").click(function() {
+    $("p#operatorsdefinition").toggle()/*removeClass("definition")*/;
+    $("h3#operatorsterm").toggle()/*addClass("disappear")*/;
   });
 
-  $(".definition").click(function() {
-    $("#operators h3").toggle("disappear");
-    $("#operators p").toggle("definition");
+  $("p#operatorsdefinition").click(function() {
+    $("h3#operatorsterm").toggle()/*removeClass("disappear")*/;
+    $("p#operatorsdefinition").toggle()/*addClass("definition")*/;
+  });
+
+
+  $("h3#functionsterm").click(function() {
+    $("p#functionsdefinition").removeClass("definition");
+    $("h3#functionsterm").addClass("disappear");
+  });
+
+  $("p#functionsdefinition").click(function() {
+    $("h3#functionsterm").removeClass("disappear");
+    $("p#functionsdefinition").addClass("definition");
   });
 
 });
